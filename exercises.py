b92f083336c198596f392a5c89fa51a8da74a0e6
Hugo Solares <hugosolaresalarcon@gmail.com>
2024-04-13 23:33:16 -0400

    adding more exercises

--- exercises.py
+++ exercises.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 class Exercises:
 
@@ -127,22 +128,158 @@ class Exercises:
             print(f'The area is = {area}')
         
         except ValueError:
+            print("Please enter valid numbers.")
+
+    # Convert a whole number in string
+    def exercise_13(self):
+        try:
+            integer = int(input('Enter a number: '))
+            print(f'the integer was: {str(integer)}, input function reads a string, so it need to be converted to int.')
+
+        except ValueError:
+            print("Please enter a valid number.")
+
+    # Replace a character in a string
+    def exercise_14(self):
+        try:
+            string = input('Enter a string: ')
+            old_char = input('Enter character to replace: ')
+            new_char = input('Enter character to replace with: ')
+
+            if len(old_char) != 1 or len(new_char) != 1:
+                raise ValueError('Please enter single characters.')
+
+            if old_char not in string:
+                raise LookupError('Character not found in string.')
+
+            string = string.replace(old_char, new_char)
+            print(f'The new string is: {string}')
+
+        except ValueError as e:
+            print(e)
+        except LookupError as e:
+            print(e)
+
+    # Turn a string to lowercase
+    def exercise_15(self):
+        try:
+            lower_string = input('Enter a string: ')
+            upper_string = lower_string.upper()
+            print(f'The uppercase string is {upper_string}')
+
+        except ValueError:
+            print('Please enter a valid number.')
+
+    # Sort a list in ascending order
+    def exercise_16(self):
+        try:
+            string = input('Enter a list of numbers separated by spaces: ')
+            # Split the string into a list of strings
+            numbers_str = string.split(' ')
+            # Convert each string in the list to an integer
+            numbers = [int(num) for num in numbers_str]
+            # sort the list
+            numbers.sort()
+            print(f'The sorted list is: {numbers}')
+
+        except ValueError:
+            print("Please enter a valid list.")
+    
+    # Calculate the power of a number without using the ** operator
+    def exercise_17(self):
+        try:
+            base = int(input('Enter base number: '))
+            power = int(input('Enter power number: '))
+            result = 1
+            for _ in range(power):
+                result = result * base
+            print(result)
+        
+        except ValueError:
+            print("Please enter valid numbers.")
+
+    # Extract a substring from a given string
+    def exercise_18(self):
+        try:
+            string = input('Enter the string: ')
+            pos_string_1 = int(input('Enter the start position: '))
+            pos_string_2 = int(input('Enter the end position: '))
+
+            if pos_string_1 < 0 or pos_string_2 > len(string):
+                raise ValueError('Positions are not valid.')
+
+            if pos_string_1 > pos_string_2:
+                raise ValueError('End position should be greater than start position.')
+
+            substring = string[pos_string_1:pos_string_2]
+            print(f'The substring is {substring}')
+
+        except ValueError as e:
+            print(e)
+    
+    # convert a float into a string
+    def exercise_19(self):
+        try:
+            number = int(input('Enter a decimal number: '))
+            print(f'The integer number is: {number}')
+        
+        except ValueError:
             print("Please enter a valid numbers.")
+
+    # Count the ocurrence of a character in a given string
+    def exercise_20(self):
+        try:
+            string = input('Enter a string: ')
+            char = input('Enter a char: ')
+            if len(char) != 1:
+                raise ValueError('Please enter single characters.')
+
+            if char not in string:
+                raise LookupError('Character not found in string.')
+            
+            count = string.count(char)
+            print(f'The number of ocurrence is: {count}')
+
+        except ValueError as e:
+            print(e)
+        
+        except LookupError as e:
+            print(e)
+
+    # Find and show the last character of a string
+    def exercise_21(self):
+        try:
+            string = input('Enter a string: ')
+            last_char = string[-1]
+            print(f'The last character is: {last_char}')
+
+        except ValueError:
+            print('Please enter a valid string.')
+        
 
 
 if __name__ == '__main__':
     exer = Exercises()
 
-    exer.exercise_1()
-    exer.exercise_2()
-    exer.exercise_3()
-    exer.exercise_4()
-    exer.exercise_5()
-    exer.exercise_6()
-    exer.exercise_7()
-    exer.exercise_8()
-    exer.exercise_9()
-    exer.exercise_10()
-    exer.exercise_11()
-    exer.exercise_12()
+    # exer.exercise_1()
+    # exer.exercise_2()
+    # exer.exercise_3()
+    # exer.exercise_4()
+    # exer.exercise_5()
+    # exer.exercise_6()
+    # exer.exercise_7()
+    # exer.exercise_8()
+    # exer.exercise_9()
+    # exer.exercise_10()
+    # exer.exercise_11()
+    # exer.exercise_12()
+    # exer.exercise_13()
+    # exer.exercise_14()
+    # exer.exercise_15()
+    # exer.exercise_16()
+    # exer.exercise_17()
+    # exer.exercise_18()
+    # exer.exercise_19()
+    # exer.exercise_20()
+    exer.exercise_21()
 
