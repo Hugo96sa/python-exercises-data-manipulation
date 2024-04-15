f1ddedd733dc46f90c187bf4cd47d20aaed544c8
Hugo Solares <hugosolaresalarcon@gmail.com>
2024-04-15 10:57:54 -0400

    adding more exercises

--- pandas_manipulation.py
+++ pandas_manipulation.py
@@ -217,7 +217,7 @@ class Manipulation:
 
     def exercise_12(self):
         sales_1_1 = self.df[(self.df['store'] == 1) & (self.df['department'] == 1)]
-        
+
         # Sort sales_1_1 by date
         sales_1_1 = sales_1_1.sort_values('date', ascending = True)
 
@@ -229,6 +229,82 @@ class Manipulation:
 
         # See the columns you calculated
         print(sales_1_1[["date", "weekly_sales", "cum_weekly_sales", "cum_max_sales"]])
+    
+    def exercise_13(self):
+        sales = self.df
+
+        # Drop duplicate store/type combinations
+        store_types = sales.drop_duplicates(subset=['store', 'type'])
+        print(store_types.head())
+
+        # Drop duplicate store/department combinations
+        store_depts = sales.drop_duplicates(subset=['store', 'department'])
+        print(store_depts.head())
+
+        # Subset the rows where is_holiday is True and drop duplicate dates
+        holiday_dates = sales[sales['is_holiday'] == True].drop_duplicates(subset='date')
+
+        # Print date col of holiday_dates
+        print(holiday_dates['date'])
+
+    def exercise_14(self):
+        sales = self.df
+
+        # Drop duplicate store/type combinations
+        store_types = sales.drop_duplicates(subset=["store", "type"])
+
+        # Drop duplicate store/department combinations
+        store_depts = sales.drop_duplicates(subset=["store", "department"])
+
+        # Count the number of stores of each type
+        store_counts = store_types['type'].value_counts()
+        print(store_counts)
+
+        # Get the proportion of stores of each type
+        store_props = store_types['type'].value_counts(normalize = True)
+        print(store_props)
+
+        # Count the number of each department number and sort
+        dept_counts_sorted = store_depts['department'].value_counts(sort = True)
+        print(dept_counts_sorted)
+
+        # Get the proportion of departments of each number and sort
+        dept_props_sorted = store_depts['department'].value_counts(sort=True, normalize=True)
+        print(dept_props_sorted)
+    
+    def exercise_15(self):
+        sales = self.df
+
+        # Calc total weekly sales
+        sales_all = sales["weekly_sales"].sum()
+
+        # Subset for type A stores, calc total weekly sales
+        sales_A = sales[sales["type"] == "A"]["weekly_sales"].sum()
+
+        # Subset for type B stores, calc total weekly sales
+        sales_B = sales[sales["type"] == "B"]["weekly_sales"].sum()
+
+        # Subset for type C stores, calc total weekly sales
+        sales_C = sales[sales["type"] == "C"]["weekly_sales"].sum()
+
+        # Get proportion for each type
+        sales_propn_by_type = [sales_A, sales_B, sales_C] / sales_all
+        print(sales_propn_by_type)
+    
+    def exercise_16(self):
+        sales = self.df
+
+        # Group by type; calc total weekly sales
+        sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+
+        # Get proportion for each type
+        sales_propn_by_type = sales_by_type / sum(sales['weekly_sales'])
+        print(sales_propn_by_type)
+
+        # Group by type and is_holiday; calc total weekly sales
+        sales_by_type_is_holiday = sales.groupby(['type', 'is_holiday'])['weekly_sales'].sum()
+        print(sales_by_type_is_holiday)
+
 
 if __name__ == '__main__':
     try: 
@@ -243,7 +319,11 @@ if __name__ == '__main__':
         # Manipulation(sales_key).exercise_9()
         # Manipulation(sales_key).exercise_10()
         # Manipulation(sales_key).exercise_11()
-        Manipulation(sales_key).exercise_12()
+        # Manipulation(sales_key).exercise_12()
+        # Manipulation(sales_key).exercise_13()
+        # Manipulation(sales_key).exercise_14()
+        # Manipulation(sales_key).exercise_15()
+        Manipulation(sales_key).exercise_16()
     
     # Handle the exceptions appropriately
     except ValueError as e:
