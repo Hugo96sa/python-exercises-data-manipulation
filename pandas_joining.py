adeb6e6b663190fd043d44b2e350e4d897a607e8
Hugo Solares <hugosolaresalarcon@gmail.com>
2024-04-19 17:10:08 -0400

    adding more exercises and comments for help

--- pandas_joining.py
+++ pandas_joining.py
@@ -1,4 +1,7 @@
+# Hugo Solares
+
 import pandas as pd
+import matplotlib.pyplot as plt
 
 actors_movies_key = 'act_mov'
 business_owners_key = 'bus_own'
@@ -52,14 +55,13 @@ world_bank_gdp_file_path = 'data/pandas_joining/WorldBank_GDP.csv'
 world_bank_pop_file_path = 'data/pandas_joining/WorldBank_POP.csv'
 zip_demo_file_path = 'data/pandas_joining/zip_demo.p'
 
-taxi = pd.read_pickle('data/pandas_joining/taxi_vehicles.p')
 
 class Joining:
     def __init__(self):
         self.df = {}
 
     def set_data(self, data_selector):
-        try: 
+        try:
             if data_selector == actors_movies_key:
                 return pd.read_csv(actors_movies_file_path)
             elif data_selector == business_owners_key:
@@ -112,7 +114,7 @@ class Joining:
                 return pd.read_pickle(zip_demo_file_path)
             else:
                 raise ValueError("Invalid data_selector provided.")
-            
+
         except FileNotFoundError:
             print(f"Error: File not found for: '{data_selector}'.")
             self.df = None
@@ -153,7 +155,7 @@ class Joining:
         print(taxi_own_veh.head())
 
         # Merge the taxi_owners and taxi_veh tables setting a suffix
-        taxi_own_veh = taxi_owners.merge(taxi_veh, on='vid', suffixes=('_own','_veh'))
+        taxi_own_veh = taxi_owners.merge(taxi_veh, on='vid', suffixes=('_own', '_veh'))
 
         # Print the value_counts to find the most popular fuel_type
         print(taxi_own_veh['fuel_type'].value_counts())
@@ -191,25 +193,94 @@ class Joining:
 
         # Print the shape of wards_census_altered
         print('wards_census_altered table shape:', wards_census_altered.shape)
-    
-    # One to many merge, 
+
+    # One to many merge, use the relationship one to many to merge different DF using one column in common
     def exercise_4(self):
         licenses = self.set_data(licenses_key)
         biz_owners = self.set_data(business_owners_key)
         print(licenses.head())
         print(biz_owners.head())
 
-        #Merge the licenses and biz_owners table on account
-        licenses_owners = licenses.merge(biz_owners,on="account")
+        # Merge the licenses and biz_owners table on account
+        licenses_owners = licenses.merge(biz_owners, on="account")
 
         # Group the results by title then count the number of accounts
-        counted_df = licenses_owners.groupby('title').agg({'account':'count'})
+        counted_df = licenses_owners.groupby('title').agg({'account': 'count'})
 
-        # Sort the counted_df in desending order
-        sorted_df = counted_df.sort_values('account',ascending=False)
+        # Sort the counted_df in descending order
+        sorted_df = counted_df.sort_values('account', ascending=False)
 
         # Use .head() method to print the first few rows of sorted_df
         print(sorted_df.head())
+
+    # Total riders in a month with a given filtering criteria
+    def exercise_5(self):
+        ridership = self.set_data(cta_ridership_key)
+        cal = self.set_data(cta_calendar_key)
+        stations = self.set_data(stations_key)
+        print(ridership.head(), ridership.shape)
+        print(cal.head(), cal.shape)
+        print(stations.head(), stations.shape)
+
+        # Merge the ridership and cal tables
+        ridership_cal = ridership.merge(cal, on=['year', 'month', 'day'])
+        print(ridership_cal.head())
+
+        # Merge the ridership, cal, and stations tables
+        ridership_cal_stations = ridership.merge(cal, on=['year', 'month', 'day']) \
+            .merge(stations, on='station_id')
+        print(ridership_cal_stations.head())
+
+        # Create a filter to filter ridership_cal_stations
+        filter_criteria = ((ridership_cal_stations['month'] == 7)
+                           & (ridership_cal_stations['day_type'] == 'Weekday')
+                           & (ridership_cal_stations['station_name'] == 'Wilson'))
+
+        # Use .loc and the filter to select for rides
+        print(ridership_cal_stations.loc[filter_criteria, 'rides'].sum())
+
+    # Three tables merge, Find median income of alderman by merging licenses and zip demo first
+    # then merging the result with wards
+    def exercise_6(self):
+        licenses = self.set_data(licenses_key)
+        zip_demo = self.set_data(zip_demo_key)
+        wards = self.set_data(ward_key)
+        print(licenses.head(), licenses.shape)
+        print(zip_demo.head(), zip_demo.shape)
+        print(wards.head(), wards.shape)
+
+        # Merge licenses and zip_demo, on zip; and merge the wards on ward
+        licenses_zip_ward = licenses.merge(zip_demo, on='zip').merge(wards, on='ward')
+
+        # Print the results by alderman and show median income
+        print(licenses_zip_ward.groupby('alderman').agg({'income': 'median'}))
+
+    # One to many merge in multople tables, Find the best location in the city to put your goat farm
+    # with a great deal of space and relatively few businesses and people around to avoid complaints
+    def exercise_7(self):
+        land_use = self.set_data(land_use_key)
+        census = self.set_data(census_key)
+        licenses = self.set_data(licenses_key)
+        print(land_use.head(), land_use.shape)
+        print(census.head(), census.shape)
+        print(licenses.head(), licenses.shape)
+
+        # Merge land_use and census and merge result with licenses including suffixes 
+        land_cen_lic = land_use.merge(census, on='ward') \
+            .merge(licenses, on='ward', suffixes=('_cen', '_lic'))
+        print(land_cen_lic.head())
+
+        # Group by ward, pop_2010, and vacant, then count the # of accounts
+        pop_vac_lic = land_cen_lic.groupby(['ward', 'pop_2010', 'vacant'],
+                                           as_index=False).agg({'account': 'count'})
+
+        # Sort pop_vac_lic and print the results
+        sorted_pop_vac_lic = pop_vac_lic.sort_values(['vacant', 'account', 'pop_2010'],
+                                                     ascending=[False, True, True])
+
+        # Print the top few rows of sorted_pop_vac_lic
+        print(sorted_pop_vac_lic.head())
+
 
 if __name__ == '__main__':
     try:
@@ -218,7 +289,10 @@ if __name__ == '__main__':
         # j.exercise_1()
         # j.exercise_2()
         # j.exercise_3()
-        j.exercise_4()
+        # j.exercise_4()
+        # j.exercise_5()
+        # j.exercise_6()
+        j.exercise_7()
 
     # Handle the exceptions appropriately
     except ValueError as e:
