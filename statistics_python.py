d80cb09087339f551ab4a6fdf8064b97eb78a174
Hugo Solares <hugosolaresalarcon@gmail.com>
2024-04-22 13:16:25 -0400

    adding more exercises and comments for help

--- statistics_python.py
+++ statistics_python.py
@@ -6,10 +6,12 @@ import matplotlib.pyplot as plt
 
 amir_deals_key = 'ami_dea'
 food_consumption_key = 'foo_con'
+restaurant_groups_key = 'res_gro'
 world_happiness_key = 'wor_hap'
 
 amir_deals_file_path = 'data/statistics_python/amir_deals.csv'
 food_consumption_file_path = 'data/statistics_python/food_consumption.csv'
+restaurant_groups_file_path = 'data/statistics_python/restaurant_groups.csv'
 world_happiness_file_path = 'data/statistics_python/world_happiness.csv'
 
 
@@ -20,9 +22,11 @@ class Statistics:
     def set_data(self, data_selector):
         try:
             if data_selector == amir_deals_key:
-                return pd.read_csv(amir_deals_file_path, index_col=0)
+                return pd.read_csv(amir_deals_file_path)
             elif data_selector == food_consumption_key:
                 return pd.read_csv(food_consumption_file_path)
+            elif data_selector == restaurant_groups_key:
+                return pd.read_csv(restaurant_groups_file_path, index_col=0)
             elif data_selector == world_happiness_key:
                 return pd.read_csv(world_happiness_file_path, index_col=0)
 
@@ -191,6 +195,49 @@ class Statistics:
         # If you randomly select one of Amir's deals, what's the probability
         # that the deal will involve Product C?: 8.427%
 
+    def exercise_8(self):
+        amir_deals = self.set_data(amir_deals_key)
+
+        # Set random seed
+        np.random.seed(24)
+
+        # Sample 5 deals without replacement
+        sample_without_replacement = amir_deals.sample(5)
+        print(sample_without_replacement)
+
+        # Sample 5 deals with replacement
+        sample_with_replacement = amir_deals.sample(5, replace=True)
+        print(sample_with_replacement)
+
+        # What type of sampling is better to use for this situation?
+        # -> Without replacement
+
+    def exercise_9(self):
+        restaurant_groups = self.set_data(restaurant_groups_key)
+
+        # Create a histogram of restaurant_groups and show plot
+        restaurant_groups['group_size'].hist(bins=np.linspace(2,6,5))
+        plt.show()
+
+        # Create probability distribution
+        size_dist = restaurant_groups['group_size'].value_counts() / restaurant_groups.shape[0]
+
+        # Reset index and rename columns
+        size_dist = size_dist.reset_index()
+        size_dist.columns = ['group_size', 'prob']
+        print(size_dist)
+
+        # Calculate expected value
+        expected_value = (size_dist['group_size'] * size_dist['prob']).sum()
+        print(expected_value)
+
+        # Subset groups of size 4 or more
+        groups_4_or_more = size_dist[size_dist['group_size'] >= 4]
+
+        # Sum the probabilities of groups_4_or_more
+        prob_4_or_more = np.sum(groups_4_or_more['prob'])
+        print(prob_4_or_more)
+
 
 if __name__ == '__main__':
     try:
@@ -202,7 +249,9 @@ if __name__ == '__main__':
         # s.exercise_4()
         # s.exercise_5()
         # s.exercise_6()
-        s.exercise_7()
+        # s.exercise_7()
+        # s.exercise_8()
+        s.exercise_9()
 
     # Handle the exceptions appropriately
     except ValueError as e:
